3460882b965c40db0ad48744323af0e16f0e9de1
5w1555 <133543220+5w1555@users.noreply.github.com>
2026-03-15 23:23:30 +0100

    Fix React Router build by moving server helpers out of routes

--- app/routes/app.additional.status.$jobId.jsx
+++ app/routes/app.additional.status.$jobId.jsx
@@ -1,12 +1,15 @@
 import { boundary } from "@shopify/shopify-app-react-router/server";
-import { authenticate } from "../shopify.server";
-import { getCropJob } from "../utils/cropJobs.server.js";
-import { createCropJobStatusLoader } from "./app.additional.status.$jobId.server.js";
 
-export const loader = createCropJobStatusLoader({
-  authenticateAdmin: authenticate.admin,
-  findCropJob: getCropJob,
-});
+export const loader = async (loaderArgs) => {
+  const { authenticate } = await import("../shopify.server");
+  const { getCropJob } = await import("../utils/cropJobs.server.js");
+  const { createCropJobStatusLoader } = await import("../services/cropJobStatusRoute.server.js");
+
+  return createCropJobStatusLoader({
+    authenticateAdmin: authenticate.admin,
+    findCropJob: getCropJob,
+  })(loaderArgs);
+};
 
 export const headers = (headersArgs) => {
   return boundary.headers(headersArgs);
