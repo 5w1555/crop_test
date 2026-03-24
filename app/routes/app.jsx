585f1afa2ea30c4e00915a11129b722d22bd8e17
5w1555 <133543220+5w1555@users.noreply.github.com>
2026-03-24 23:58:58 +0100

    Fix crop route structure

---
+++ app/routes/app.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router";
+import { authenticate } from "../shopify.server";
+
+export const loader = async ({ request }) => {
+  await authenticate.admin(request);
+  return null;
+};
+
+export default function AppShell() {
+  return <Outlet />;
+}
