024e261a03c5c804c5d89a279523b70e51768f47
Marwan Wafik <Marwanewafik@hotmail.fr>
2026-03-25 13:54:44 +0100

    h

--- app/routes/auth.login.jsx
+++ app/routes/auth.login.jsx
@@ -2,7 +2,7 @@ import { AppProvider } from "@shopify/shopify-app-react-router/react";
 import { useState } from "react";
 import { Form, useActionData, useLoaderData } from "react-router";
 import { login } from "../shopify.server.js";
-import { loginErrorMessage } from "./auth.error.server.jsx";
+import { loginErrorMessage } from "./auth.error.jsx";
 
 export const loader = async ({ request }) => {
   const errors = loginErrorMessage(await login(request));
