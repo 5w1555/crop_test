c1c6d6872f563db5fc992fda64eabdc89a7a7949
Marwan Wafik <Marwanewafik@hotmail.fr>
2026-03-26 15:37:27 +0100

    fixing app.jsx

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -1,6 +1,6 @@
 import { Link, Outlet, useLoaderData, useRouteError } from "react-router";
-import { boundary } from "@shopify/shopify-app-remix/server";
-import { AppProvider } from "@shopify/shopify-app-remix/react";
+import { boundary } from "@shopify/shopify-app-react-router/server";
+import { AppProvider } from "@shopify/shopify-app-react-router/react"
 import { NavMenu } from "@shopify/app-bridge-react";
 import { authenticate } from "../shopify.server";
 
